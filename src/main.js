06c90484b5bc720d9eebd7161ed5507912138555
Maddyrulz45 <130278402+Maddyrulz45@users.noreply.github.com>
2023-04-21 16:45:22 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,14 +15,13 @@ const newResults = new Set();
 const houses = [];
 const { CHAT_ID, BOT_API } = process.env;
 
-const urls = [
-    'https://www.funda.nl/en/huur/gemeente-utrecht/beschikbaar/0-1500/3-dagen/sorteer-datum-af/',
-    'https://www.funda.nl/en/huur/gemeente-utrecht/beschikbaar/0-1500/3-dagen/sorteer-datum-af/',
+const parariusURL  = [
+    'https://www.pararius.com/apartments/utrecht/200-1500/since-1'
 ];
 
-const runTask = async () => {
-    for (const url of urls) {
-        await runPuppeteer(url);
+const runTaskforPararius = async () => {
+    for (const url of parariusURL ) {
+        await runPuppeteerforPararius(url);
     }
 
     console.log('newResults:', newResults);
@@ -37,46 +36,12 @@ const runTask = async () => {
         const date = (new Date()).toISOString().split('T')[0];
         houses.forEach(({
             path,
-            income,
-            residentsAge0to14,
-            residentsAge15to24,
-            residentsAge25to44,
-            residentsAge45to64,
-            residentsAge65AndOlder,
-            householdsWithChildren,
-            shareOfMorocco,
-            shareOfAntillesOrAruba,
-            shareOfSuriname,
-            shareOfTurkey,
-            neighbourhoodName,
-            municipalityName,
-            shareOfNonImmigrants,
-            residentsCount,
-            totalImmigrantsCount,
+            subtitleText,
+            zipCode
         }) => {
             let text = `New house on ${date}: [click here](${path})`;
+            text = `${text}\n${subtitleText}\n${zipCode}`;
 
-            if (income) {
-                let extraStuff = `
-residentsIncome: **${income}**
-neighbourhoodName: **${neighbourhoodName}**
-municipalityName: **${municipalityName}**
-residentsAge0to14: **${residentsAge0to14}**
-residentsAge15to24: **${residentsAge15to24}**
-residentsAge25to44: **${residentsAge25to44}**
-residentsAge45to64: **${residentsAge45to64}**
-residentsAge65AndOlder: **${residentsAge65AndOlder}**
-householdsWithChildren: **${householdsWithChildren}**
-residentsCount: **${residentsCount}**
-totalImmigrantsCount: **${totalImmigrantsCount}**
-shareOfNonImmigrants: **${shareOfNonImmigrants}**
-shareOfMorocco: **${shareOfMorocco}**
-shareOfAntillesOrAruba: **${shareOfAntillesOrAruba}**
-shareOfSuriname: **${shareOfSuriname}**
-shareOfTurkey: **${shareOfTurkey}**
-`;
-                text = `${text}\n${extraStuff}`;
-            }
 
             nodeFetch(`https://api.telegram.org/bot${BOT_API}/sendMessage`, {
                 method: 'POST',
@@ -93,7 +58,7 @@ shareOfTurkey: **${shareOfTurkey}**
     }
 };
 
-const runPuppeteer = async (url) => {
+const runPuppeteerforPararius = async (url) => {
     console.log('opening headless browser');
     const browser = await puppeteer.launch({
         headless: true,
@@ -108,23 +73,27 @@ const runPuppeteer = async (url) => {
     // https://stackoverflow.com/a/51732046/4307769 https://stackoverflow.com/a/68780400/4307769
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36');
 
-    console.log('going to funda');
+    console.log('going to Pararius');
     await page.goto(url, { waitUntil: 'domcontentloaded' });
 
     const htmlString = await page.content();
     const dom = new jsdom.JSDOM(htmlString);
 
 
-    console.log('parsing funda.nl data');
-    const result = dom.window.document.querySelectorAll('.search-result');
+    console.log('parsing pararius.nl data');
+    
+    // set the length to 0
+    houses.length = 0;
+    const result = dom.window.document.querySelectorAll('.search-list');// add the result set class
+    
     for (const element of result) {
         const urlPath = element?.querySelectorAll('a')?.[0]?.href;
-        const headerSubtitle = element?.querySelector('.search-result__header-subtitle');
+        const headerSubtitle = element?.querySelector('.listing-search-item__link--title'); //listing-search-item__title
         const subtitleText = headerSubtitle?.innerHTML?.trim();
 
         let path = urlPath;
-        if (!path.includes('https://www.funda.nl')) {
-            path = `https://www.funda.nl${urlPath}`;
+        if (!path.includes('https://www.pararius.com')) { // add new site url
+            path = `https://www.pararius.com${urlPath}`;
         }
 
         path = path.replace('?navigateSource=resultlist', '');
@@ -132,41 +101,11 @@ const runPuppeteer = async (url) => {
             let extraDetails = {};
             const zipCode = getZipCode(subtitleText || '');
 
-            if (zipCode) {
-                const neighbourhoodData = await getNeighbourhoodData(zipCode);
-
-                if (neighbourhoodData) {
-                    const residentsCount = neighbourhoodData?.['AantalInwoners_5']?.value || 0;
-                    const westernImmigrantsCount = neighbourhoodData?.['WestersTotaal_17']?.value || 0;
-                    const nonWesternImmigrantsCount = neighbourhoodData?.['NietWestersTotaal_18']?.value || 0;
-                    const totalImmigrantsCount = westernImmigrantsCount + nonWesternImmigrantsCount;
-                    const income = neighbourhoodData?.['GemiddeldInkomenPerInwoner_66']?.value * 1000;
-
-                    extraDetails = {
-                        ...extraDetails,
-                        income,
-                        residentsAge0to14: neighbourhoodData['k_0Tot15Jaar_8'].value,
-                        residentsAge15to24: neighbourhoodData['k_15Tot25Jaar_9'].value,
-                        residentsAge25to44: neighbourhoodData['k_25Tot45Jaar_10'].value,
-                        residentsAge45to64: neighbourhoodData['k_45Tot65Jaar_11'].value,
-                        residentsAge65AndOlder: neighbourhoodData['k_65JaarOfOuder_12'].value,
-                        householdsWithChildren: neighbourhoodData['HuishoudensMetKinderen_31'].value,
-                        totalImmigrantsCount,
-                        shareOfMorocco: convertResidentsToPercentage(residentsCount, neighbourhoodData['Marokko_19'].value),
-                        shareOfAntillesOrAruba: convertResidentsToPercentage(residentsCount, neighbourhoodData['NederlandseAntillenEnAruba_20'].value),
-                        shareOfSuriname: convertResidentsToPercentage(residentsCount, neighbourhoodData['Suriname_21'].value),
-                        shareOfTurkey: convertResidentsToPercentage(residentsCount, neighbourhoodData['Turkije_22'].value),
-                        shareOfNonImmigrants: convertResidentsToPercentage(residentsCount, residentsCount - totalImmigrantsCount),
-                        neighbourhoodName: neighbourhoodData.neighbourhoodName.value,
-                        municipalityName: neighbourhoodData.municipalityName.value,
-                        residentsCount,
-                    };
-                }
-            }
-
             newResults.add(path);
+            
             houses.push({
-                ...extraDetails,
+                subtitleText,
+                zipCode,
                 path,
             });
         }
@@ -177,7 +116,7 @@ const runPuppeteer = async (url) => {
 };
 
 if (CHAT_ID && BOT_API) {
-    runTask();
+    runTaskforPararius();
 } else {
     console.log('Missing Telegram API keys!');
 }
